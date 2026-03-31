5bbd5d741370308ba9180ddfe00d0b279c4b0553
ra100 <git@ra100.net>
2026-03-31 21:02:09 +0200

    🔧 Update Facebook event URL and correct feedback form description for SlavCon 2026

--- src/slavconData.ts
+++ src/slavconData.ts
@@ -3,7 +3,7 @@ export const slavconData = {
   description:
     'SlavCon 2026 je 19. ročník medzinárodného trojdňového festivalu fantasy, sci-fi, histórie, mytológie a popkultúry. Je určený pre všetkých fanúšikov literatúry, filmu, seriálov, hier, histórie, larpu a výtvarného umenia.',
   webUrl: 'https://slavcon.sk/',
-  fbUrl: 'https://www.facebook.com/events/478961378453750',
+  fbUrl: 'https://www.facebook.com/events/1945079429390629',
   parallelEventsRooms: ['']
 }
 
@@ -13,7 +13,7 @@ export const floorPlan = [
 
 export const feedbackForm = {
   title: 'SlavCon 2026 - ako sa vám páčilo?',
-  description: 'DSlavCon 2026 - ako sa vám páčilo?',
+  description: 'SlavCon 2026 - ako sa vám páčilo?',
   image: '',
   link: 'https://slavcon.sk/feedback',
   enabled: true
